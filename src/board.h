9eacfd3a738290e322594d797ef3e8a7d05fed67
schwestermutter1 <schwestermutter1@gmail.com>
2024-05-22 21:50:11 +0700

    display cell num

---
+++ src/board.h
@@ -0,0 +1,7 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+// -1 = empty cell
+typedef int board[81];
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <raylib.h>
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "board.h"
 
 #define BOARD_SIZE 549
 #define CELL_SIZE BOARD_SIZE / 9
 
 int main()
 {
+    board board;
+    for (int i = 0; i < 81; i++) board[i] = -1;
+
     size_t s_cell[2] = { 0, 0 };
     
     InitWindow(BOARD_SIZE, BOARD_SIZE, "sudoku solver");
@@ -23,7 +31,31 @@ int main()
         for (int x = 0; x <= BOARD_SIZE; x += CELL_SIZE) DrawLine(x, 0, x, BOARD_SIZE, BLACK);
         for (int y = 0; y <= BOARD_SIZE; y += CELL_SIZE) DrawLine(0, y, BOARD_SIZE, y, BLACK);
 
-        DrawRectangleLines(s_cell[0] * CELL_SIZE, s_cell[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE, BLUE);
+        for (int i = 0; i < 81; i++) {
+            int num = board[i];
+
+            if (num == -1) continue;
+
+            char chr[2];
+            sprintf(chr, "%d", num);
+
+            int x = i % 9;
+            int y = (i - x) / 9;
+
+            int sx = x * CELL_SIZE;
+            int sy = y * CELL_SIZE;
+
+            DrawText(chr, sx, sy, 50, BLACK);
+        }
+
+        // draw selected cell
+        DrawRectangleLines(
+            s_cell[0] * CELL_SIZE,
+            s_cell[1] * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE,
+            BLUE
+        );
         
         EndDrawing();
     }
