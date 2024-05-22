eb6a95f558e2ede4c767eff2bb1de5c86f2c71b9
schwestermutter1 <schwestermutter1@gmail.com>
2024-05-22 20:13:47 +0700

    add gridline

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include <raylib.h>
 #include <stdlib.h>
 
+#define WIDTH 594
+#define HEIGHT 594
+
 int main()
 {
-    InitWindow(600, 600, "sudoku solver");
+    InitWindow(WIDTH, HEIGHT, "sudoku solver");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+
+        for (int x = 0; x <= WIDTH; x += WIDTH / 9) DrawLine(x, 0, x, HEIGHT, BLACK);
+        for (int y = 0; y <= HEIGHT; y += HEIGHT / 9) DrawLine(0, y, WIDTH, y, BLACK);
         
         EndDrawing();
     }
